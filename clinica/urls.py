deb02bab3ffb76b904c2a7d1479269ba21d73f26
Camilo Castiblanco <jhoancamilo0812@hotmail.com>
2025-04-08 20:24:14 -0500

    cambios en los formularios y botones

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('pacientes/', views.lista_pacientes, name='lista_pacientes'),
@@ -7,4 +9,6 @@ urlpatterns = [
     path('login/', views.iniciar_sesion, name='login'),
     path('editar-historial/', views.editar_historial, name='editar_historial'),
     path('buscar-pacientes/', views.buscar_pacientes, name='buscar_pacientes'),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
+    path('', lambda request: redirect('login/')),
 ]
